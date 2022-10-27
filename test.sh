9bc885e0387410a2aa88d150312b367f8094248d
MatteoDelfavero <matteo.delfavero@gmail.com>
2022-10-27 15:02:28 +0200

    1

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt --fix-missing update
-sudo apt clean
-sudo apt autoremove
-sudo apt update
-sudo apt full-upgrade -y
+for pkg in `dpkg --get-selections | awk '{print $1}' | egrep -v '(dpkg|apt|mysql|mythtv)'` ; do sudo apt-get -y --force-yes install --reinstall $pkg ; done
+
+
+
+
+# sudo apt --fix-missing update
+# sudo apt clean
+# sudo apt autoremove
+# sudo apt update
+# sudo apt full-upgrade -y
